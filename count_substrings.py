4ed02eacc8d50497823aae9819dbf11235b43f88
DrDubel <antek.suszko@gmail.com>
2023-08-23 11:42:52 +0200

    bunch of small improvements

--- count_substrings.py
+++ count_substrings.py
@@ -4,38 +4,42 @@ import pickle
 from string import ascii_lowercase
 
 
-def read_dicts() -> tuple:
+def read_dicts():
     de = open("data/simp_de.dic").read().split()
     es = open("data/simp_es.dic").read().split()
     en = open("data/simp_en.dic").read().split()
     pl = open("data/simp_pl.dic").read().split()
-    return (de, es, en, pl)
+    lde = len("".join(de))
+    les = len("".join(es))
+    lenn = len("".join(en))
+    lpl = len("".join(pl))
+    return ((de, es, en, pl), (lde, les, lenn, lpl))
 
 
-def count_substring(substr, de, es, en, pl):
-    dec = len(re.findall(substr, "\n".join(de)))
-    esc = len(re.findall(substr, "\n".join(es)))
-    enc = len(re.findall(substr, "\n".join(en)))
-    plc = len(re.findall(substr, "\n".join(pl)))
+def count_substring(substr, de, es, en, pl, lde, les, lenn, lpl):
+    sum_len = sum([lde, les, lenn, lpl])
+    dec = len(re.findall(substr, "\n".join(de))) / (lde / sum_len * 100)
+    esc = len(re.findall(substr, "\n".join(es))) / (les / sum_len * 100)
+    enc = len(re.findall(substr, "\n".join(en))) / (lenn / sum_len * 100)
+    plc = len(re.findall(substr, "\n".join(pl))) / (lpl / sum_len * 100)
     return (dec, esc, enc, plc)
 
 
-def count_substrings(langs, substrs, debug=False):
+def count_substrings(substrs, langs, lens, debug=False):
     for substr in substrs:
-        langcs = count_substring(substr, *langs)
+        langcs = count_substring(substr, *langs, *lens)
         langpercs = [
-            round(langc / sum(langcs, 2) * 100, 3) if sum(langcs) else 0
-            for langc in langcs
+            round(langc / sum(langcs), 4) if sum(langcs) else 0 for langc in langcs
         ]
         if debug:
             print(
                 """
 Susbtring: {0}
 -----------
-de: {1}% - {5}
-es: {2}% - {6}
-en: {3}% - {7}
-pl: {4}% - {8}
+de: {1:%} - {5}
+es: {2:%} - {6}
+en: {3:%} - {7}
+pl: {4:%} - {8}
 -----------""".format(
                     substr, *langpercs, *langcs
                 )
@@ -44,9 +48,9 @@ pl: {4}% - {8}
 
 
 def main():
-    langs = read_dicts()
-    substrs = [i + j for i in ascii_lowercase[:4] for j in ascii_lowercase[:5]]
-    probs = dict(tuple(count_substrings(langs, substrs)))
+    langs, lens = read_dicts()
+    substrs = [i + j for i in ascii_lowercase for j in ascii_lowercase]
+    probs = dict(tuple(count_substrings(substrs, langs, lens, debug=True)))
     with open("data/probs.pickle", "wb") as f:
         pickle.dump(probs, f)
     print(probs)

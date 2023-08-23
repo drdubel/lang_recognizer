87c93d28fd9695ac1fd30315ad126ba8cbbd0a40
DrDubel <antek.suszko@gmail.com>
2023-08-23 12:35:05 +0200

    big time improvement

--- check_lang.py
+++ check_lang.py
@@ -1,26 +1,20 @@
-import re
 import pickle
 
 
 def check_word(word, probs):
     lang_probs = [1, 1, 1, 1]
-    for substr, prob in probs.items():
-        if re.findall(substr, word):
-            lang_probs = [i * j for i, j in zip(prob, lang_probs)]
-            lang_probs = [
-                round(langc / sum(lang_probs), 4) if sum(lang_probs) else 0
-                for langc in lang_probs
-            ]
+    for substr in [word[i : i + 2] for i in range(len(word) - 1)]:
+        lang_probs = [i * j for i, j in zip(probs[substr], lang_probs)]
+        lang_probs = [
+            round(langc / sum(lang_probs), 4) if sum(lang_probs) else 0
+            for langc in lang_probs
+        ]
     return lang_probs
 
 
-def main():
-    probs = pickle.load(open("data/probs.pickle", "rb"))
-    while True:
-        word = input("Give word: ")
-        lang_probs = check_word(word, probs)
-        print(
-            """
+def pretty_print_stats(word, lang_probs):
+    print(
+        """
 Word: {0}
 -----------
 de: {1:%}
@@ -28,9 +22,19 @@ es: {2:%}
 en: {3:%}
 pl: {4:%}
 -----------""".format(
-                word, *[lang_prob for lang_prob in lang_probs]
-            )
+            word, *[lang_prob for lang_prob in lang_probs]
         )
+    )
+
+
+def main():
+    n = int(input())
+    probs = pickle.load(open("data/probs.pickle", "rb"))
+
+    for _ in range(n):
+        word = input("Give word: ")
+        lang_probs = check_word(word, probs)
+        print(("de", "es", "en", "pl")[lang_probs.index(max(lang_probs))])
 
 
 if __name__ == "__main__":

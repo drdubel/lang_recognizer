1d84caadff6cdb523d2961f878abd00f1adb49e0
DrDubel <antek.suszko@gmail.com>
2023-08-23 14:01:18 +0200

    first and last character counting

--- check_lang.py
+++ check_lang.py
@@ -3,6 +3,12 @@ import pickle
 
 def check_word(word, probs):
     lang_probs = [1, 1, 1, 1]
+    lang_probs = [i * j for i, j in zip(probs["^" + word[0]], lang_probs)]
+    lang_probs = [i * j for i, j in zip(probs[word[-1] + "$"], lang_probs)]
+    lang_probs = [
+        round(langc / sum(lang_probs), 4) if sum(lang_probs) else 0
+        for langc in lang_probs
+    ]
     for substr in [word[i : i + 2] for i in range(len(word) - 1)]:
         lang_probs = [i * j for i, j in zip(probs[substr], lang_probs)]
         lang_probs = [

c2e5d95714c1cf30af34e2a1318bb0d566c11dfb
DrDubel <antek.suszko@gmail.com>
2023-08-23 16:47:18 +0200

    better substring searching

--- check_lang.py
+++ check_lang.py
@@ -3,14 +3,48 @@ import pickle
 
 def check_word(word, probs):
     lang_probs = [1, 1, 1, 1]
+    if len(word) == 1:
+        return probs[word]
     lang_probs = [i * j for i, j in zip(probs["^" + word[0]], lang_probs)]
     lang_probs = [i * j for i, j in zip(probs[word[-1] + "$"], lang_probs)]
     lang_probs = [
         round(langc / sum(lang_probs), 4) if sum(lang_probs) else 0
         for langc in lang_probs
     ]
-    for substr in [word[i : i + 2] for i in range(len(word) - 1)]:
-        lang_probs = [i * j for i, j in zip(probs[substr], lang_probs)]
+    for k in range(len(word)):
+        if len(word) - k >= 2:
+            lang_probs = [i * j for i, j in zip(probs[word[k : k + 2]], lang_probs)]
+        if len(word) - k == 2:
+            if word[k : k + 2] + "$" in probs:
+                lang_probs = [
+                    i * j for i, j in zip(probs[word[k : k + 2] + "$"], lang_probs)
+                ]
+        if len(word) - k == 3:
+            if word[k : k + 3] + "$" in probs:
+                lang_probs = [
+                    i * j for i, j in zip(probs[word[k : k + 3] + "$"], lang_probs)
+                ]
+        if len(word) - k == 4:
+            if word[k : k + 4] + "$" in probs:
+                lang_probs = [
+                    i * j for i, j in zip(probs[word[k : k + 4] + "$"], lang_probs)
+                ]
+        if k == 0:
+            if len(word) >= 4:
+                if "^" + word[0:4] in probs:
+                    lang_probs = [
+                        i * j for i, j in zip(probs["^" + word[0:4]], lang_probs)
+                    ]
+            if len(word) >= 3:
+                if "^" + word[0:3] in probs:
+                    lang_probs = [
+                        i * j for i, j in zip(probs["^" + word[0:3]], lang_probs)
+                    ]
+            if len(word) >= 2:
+                if "^" + word[0:2] in probs:
+                    lang_probs = [
+                        i * j for i, j in zip(probs["^" + word[0:2]], lang_probs)
+                    ]
         lang_probs = [
             round(langc / sum(lang_probs), 4) if sum(lang_probs) else 0
             for langc in lang_probs
